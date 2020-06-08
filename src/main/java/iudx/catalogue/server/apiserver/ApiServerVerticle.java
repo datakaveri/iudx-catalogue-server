83a2a861d368b1fbc8515e7557c752b302ddd336
unknown <karun.singh@datakaveri.org>
2020-06-08 17:02:19 +0530

    api server vertical updated with initial attribute and config APIs

--- src/main/java/iudx/catalogue/server/apiserver/ApiServerVerticle.java
+++ src/main/java/iudx/catalogue/server/apiserver/ApiServerVerticle.java
@@ -1,15 +1,21 @@
 package iudx.catalogue.server.apiserver;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.MultiMap;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.net.JksOptions;
 import io.vertx.core.spi.cluster.ClusterManager;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.servicediscovery.ServiceDiscovery;
 import io.vertx.servicediscovery.types.EventBusService;
@@ -26,8 +32,9 @@ import java.util.Properties;
  * The Catalogue Server API Verticle.
  * <h1>Catalogue Server API Verticle</h1>
  * <p>
- * The API Server verticle implements the IUDX Catalogue Server APIs. It handles the API requests
- * from the clients and interacts with the associated Service to respond.
+ * The API Server verticle implements the IUDX Catalogue Server APIs. It handles
+ * the API requests from the clients and interacts with the associated Service
+ * to respond.
  * </p>
  * 
  * @see io.vertx.core.Vertx
@@ -43,141 +50,310 @@ import java.util.Properties;
 
 public class ApiServerVerticle extends AbstractVerticle {
 
-  private static final Logger logger = LoggerFactory.getLogger(ApiServerVerticle.class);
-  private Vertx vertx;
-  private ClusterManager mgr;
-  private VertxOptions options;
-  private ServiceDiscovery discovery;
-  private DatabaseService database;
-  private OnboarderService onboarder;
-  private ValidatorService validator;
-  private AuthenticationService authenticator;
-  private HttpServer server;
-  private Router router;
-  private Properties properties;
-  private InputStream inputstream;
-  private final int port = 8443;
-  private String keystore;
-  private String keystorePassword;
+	private static final Logger logger = LoggerFactory.getLogger(ApiServerVerticle.class);
+	private Vertx vertx;
+	private ClusterManager mgr;
+	private VertxOptions options;
+	private ServiceDiscovery discovery;
+	private DatabaseService database;
+	private OnboarderService onboarder;
+	private ValidatorService validator;
+	private AuthenticationService authenticator;
+	private HttpServer server;
+	private Router router;
+	private Properties properties;
+	private InputStream inputstream;
+	private final int port = 8443;
+	private String keystore;
+	private String keystorePassword;
 
-  /**
-   * This method is used to start the Verticle. It deploys a verticle in a cluster, reads the
-   * configuration, obtains a proxy for the Event bus services exposed through service discovery,
-   * start an HTTPs server at port 8443.
-   * 
-   * @throws Exception which is a startup exception
-   */
+	/**
+	 * This method is used to start the Verticle. It deploys a verticle in a
+	 * cluster, reads the configuration, obtains a proxy for the Event bus services
+	 * exposed through service discovery, start an HTTPs server at port 8443.
+	 * 
+	 * @throws Exception which is a startup exception
+	 */
 
-  @Override
-  public void start() throws Exception {
+//	public static void main(String args[]) {
+//		JsonObject test = new JsonObject();
+//		test.put("testing", 1).put("another", 2);
+//		System.out.println(test.toString());
+//		
+//		String[] id = "value true".split(",");
+//		System.out.println(id[0]);
+//		System.out.println("++++++++");
+//		System.out.println(id.length);
+//		
+//		JsonArray value = new JsonArray();
+//		String[] split = ("property,parameter,value").split(",");
+//		for (String str : split) {
+//			value.add(str);
+//		}
+//		JsonObject tests= new JsonObject();
+//		tests.put("test", value);
+//		System.out.println(tests.toString());
+//		
+//	}
 
-    /** Create a reference to HazelcastClusterManager. */
+	@Override
+	public void start() throws Exception {
 
-    mgr = new HazelcastClusterManager();
-    options = new VertxOptions().setClusterManager(mgr);
+		/** Create a reference to HazelcastClusterManager. */
 
-    /** Create or Join a Vert.x Cluster. */
+		mgr = new HazelcastClusterManager();
+		options = new VertxOptions().setClusterManager(mgr);
 
-    Vertx.clusteredVertx(options, res -> {
-      if (res.succeeded()) {
+		/** Create or Join a Vert.x Cluster. */
 
-        vertx = res.result();
-        router = Router.router(vertx);
-        properties = new Properties();
-        inputstream = null;
+		Vertx.clusteredVertx(options, res -> {
+			if (res.succeeded()) {
 
-        /** Define the APIs, methods, endpoints and associated methods. */
+				vertx = res.result();
+				router = Router.router(vertx);
+				properties = new Properties();
+				inputstream = null;
 
-        Router router = Router.router(vertx);
-        router.route("/apis/*").handler(StaticHandler.create());
+				/** Define the APIs, methods, endpoints and associated methods. */
 
-        /** Read the configuration and set the HTTPs server properties. */
+				Router router = Router.router(vertx);
+				router.route("/apis/*").handler(StaticHandler.create());
+				router.get("/iudx/cat/v1/search").handler(this::search);
+				router.get("/iudx/cat/v1/ui/cities").handler(this::getCities);
+				router.get("/iudx/cat/v1/ui/config").handler(this::getConfig);
+				router.post("/iudx/cat/v1/ui/config").handler(this::setConfig);
+				router.delete("/iudx/cat/v1/ui/config").handler(this::deleteConfig);
+				router.put("/iudx/cat/v1/ui/config").handler(this::updateConfig);
+				router.patch("/iudx/cat/v1/ui/config").handler(this::appendConfig);
 
-        try {
+				/** Read the configuration and set the HTTPs server properties. */
 
-          inputstream = new FileInputStream("config.properties");
-          properties.load(inputstream);
+				try {
 
-          keystore = properties.getProperty("keystore");
-          keystorePassword = properties.getProperty("keystorePassword");
+					inputstream = new FileInputStream("config.properties");
+					properties.load(inputstream);
 
-        } catch (Exception ex) {
+					keystore = properties.getProperty("keystore");
+					keystorePassword = properties.getProperty("keystorePassword");
 
-          logger.info(ex.toString());
+				} catch (Exception ex) {
 
-        }
+					logger.info(ex.toString());
 
-        /** Setup the HTTPs server properties, APIs and port. */
+				}
 
-        server = vertx.createHttpServer(new HttpServerOptions().setSsl(true)
-            .setKeyStoreOptions(new JksOptions().setPath(keystore).setPassword(keystorePassword)));
+				/** Setup the HTTPs server properties, APIs and port. */
 
-        server.requestHandler(router).listen(port);
+				server = vertx.createHttpServer(new HttpServerOptions().setSsl(true)
+						.setKeyStoreOptions(new JksOptions().setPath(keystore).setPassword(keystorePassword)));
 
-        /** Get a handler for the Service Discovery interface. */
+				server.requestHandler(router).listen(port);
 
-        discovery = ServiceDiscovery.create(vertx);
+				/** Get a handler for the Service Discovery interface. */
 
-        /** Get a handler for the DatabaseService from Service Discovery interface. */
+				discovery = ServiceDiscovery.create(vertx);
 
-        EventBusService.getProxy(discovery, DatabaseService.class,
-            databaseServiceDiscoveryHandler -> {
-              if (databaseServiceDiscoveryHandler.succeeded()) {
-                database = databaseServiceDiscoveryHandler.result();
-                logger.info(
-                    "\n +++++++ Service Discovery  Success. +++++++ \n +++++++ Service name is : "
-                        + database.getClass().getName() + " +++++++ ");
-              } else {
-                logger.info("\n +++++++ Service Discovery Failed. +++++++ ");
-              }
-            });
+				/** Get a handler for the DatabaseService from Service Discovery interface. */
 
-        /** Get a handler for the OnboarderService from Service Discovery interface. */
+				EventBusService.getProxy(discovery, DatabaseService.class, databaseServiceDiscoveryHandler -> {
+					if (databaseServiceDiscoveryHandler.succeeded()) {
+						database = databaseServiceDiscoveryHandler.result();
+						logger.info("\n +++++++ Service Discovery  Success. +++++++ \n +++++++ Service name is : "
+								+ database.getClass().getName() + " +++++++ ");
+					} else {
+						logger.info("\n +++++++ Service Discovery Failed. +++++++ ");
+					}
+				});
 
-        EventBusService.getProxy(discovery, OnboarderService.class,
-            onboarderServiceDiscoveryHandler -> {
-              if (onboarderServiceDiscoveryHandler.succeeded()) {
-                onboarder = onboarderServiceDiscoveryHandler.result();
-                logger.info(
-                    "\n +++++++ Service Discovery  Success. +++++++ \n +++++++ Service name is : "
-                        + onboarder.getClass().getName() + " +++++++ ");
-              } else {
-                logger.info("\n +++++++ Service Discovery Failed. +++++++ ");
-              }
-            });
+				/** Get a handler for the OnboarderService from Service Discovery interface. */
 
-        /** Get a handler for the ValidatorService from Service Discovery interface. */
+				EventBusService.getProxy(discovery, OnboarderService.class, onboarderServiceDiscoveryHandler -> {
+					if (onboarderServiceDiscoveryHandler.succeeded()) {
+						onboarder = onboarderServiceDiscoveryHandler.result();
+						logger.info("\n +++++++ Service Discovery  Success. +++++++ \n +++++++ Service name is : "
+								+ onboarder.getClass().getName() + " +++++++ ");
+					} else {
+						logger.info("\n +++++++ Service Discovery Failed. +++++++ ");
+					}
+				});
 
-        EventBusService.getProxy(discovery, ValidatorService.class,
-            validatorServiceDiscoveryHandler -> {
-              if (validatorServiceDiscoveryHandler.succeeded()) {
-                validator = validatorServiceDiscoveryHandler.result();
-                logger.info(
-                    "\n +++++++ Service Discovery  Success. +++++++ \n +++++++ Service name is : "
-                        + validator.getClass().getName() + " +++++++ ");
-              } else {
-                logger.info("\n +++++++ Service Discovery Failed. +++++++ ");
-              }
-            });
+				/** Get a handler for the ValidatorService from Service Discovery interface. */
 
-        /** Get a handler for the AuthenticationService from Service Discovery interface. */
+				EventBusService.getProxy(discovery, ValidatorService.class, validatorServiceDiscoveryHandler -> {
+					if (validatorServiceDiscoveryHandler.succeeded()) {
+						validator = validatorServiceDiscoveryHandler.result();
+						logger.info("\n +++++++ Service Discovery  Success. +++++++ \n +++++++ Service name is : "
+								+ validator.getClass().getName() + " +++++++ ");
+					} else {
+						logger.info("\n +++++++ Service Discovery Failed. +++++++ ");
+					}
+				});
 
-        EventBusService.getProxy(discovery, AuthenticationService.class,
-            authenticatorServiceDiscoveryHandler -> {
-              if (authenticatorServiceDiscoveryHandler.succeeded()) {
-                authenticator = authenticatorServiceDiscoveryHandler.result();
-                logger.info(
-                    "\n +++++++ Service Discovery  Success. +++++++ \n +++++++ Service name is : "
-                        + authenticator.getClass().getName() + " +++++++ ");
-              } else {
-                logger.info("\n +++++++ Service Discovery Failed. +++++++ ");
-              }
-            });
+				/**
+				 * Get a handler for the AuthenticationService from Service Discovery interface.
+				 */
 
-      }
-    });
+				EventBusService.getProxy(discovery, AuthenticationService.class,
+						authenticatorServiceDiscoveryHandler -> {
+							if (authenticatorServiceDiscoveryHandler.succeeded()) {
+								authenticator = authenticatorServiceDiscoveryHandler.result();
+								logger.info(
+										"\n +++++++ Service Discovery  Success. +++++++ \n +++++++ Service name is : "
+												+ authenticator.getClass().getName() + " +++++++ ");
+							} else {
+								logger.info("\n +++++++ Service Discovery Failed. +++++++ ");
+							}
+						});
 
+			}
+		});
 
-  }
+	}
 
+	public void search(RoutingContext routingContext) {
+		HttpServerRequest request = routingContext.request();
+		HttpServerResponse response = routingContext.response();
+		System.out.println("routed to search");
+		MultiMap params = request.params();
+		System.out.println("value= " + params.names());
+
+		if (request.getParam("property") == null || request.getParam("value") == null) {
+			response.setStatusCode(400);
+			JsonObject json = new JsonObject();
+			json.put("status", "invalidSyntax").put("results", new JsonArray());
+			response.headers().add("Content-Type", "JSON").add("Content-Length",
+					String.valueOf(json.toString().length()));
+			response.write(json.toString());
+			response.end();
+			return;
+		}
+
+		JsonArray value = new JsonArray();
+		String[] split = request.getParam("value").split(",");
+		for (String str : split) {
+			value.add(str);
+		}
+		JsonObject queryJson = new JsonObject();
+		queryJson.put("property", request.getParam("property"));
+		queryJson.put("value", value);
+		// Query queryJson to Database
+		JsonObject resultJson = new JsonObject();
+		// store response from DB to resultJson
+		String status = resultJson.getString("status");
+		if (status.equalsIgnoreCase("success")) {
+			response.setStatusCode(200);
+		} else if (status.equalsIgnoreCase("partial-content")) {
+			response.setStatusCode(206);
+		} else {
+			response.setStatusCode(400);
+		}
+		response.headers().add("Content-Type", "JSON").add("Content-Length",
+				String.valueOf(resultJson.toString().length()));
+		response.write(resultJson.toString());
+		response.end();
+	}
+
+	public void getCities(RoutingContext routingContext) {
+		HttpServerRequest request = routingContext.request();
+		HttpServerResponse response = routingContext.response();
+		// Query database for all cities
+		JsonObject resultJson = new JsonObject();
+		// Store response from DB to resultJson
+		response.setStatusCode(200);
+		response.headers().add("Content-Type", "JSON").add("Content-Length",
+				String.valueOf(resultJson.toString().length()));
+		response.write(resultJson.toString());
+		response.end();
+	}
+
+	public void getConfig(RoutingContext routingContext) {
+		HttpServerRequest request = routingContext.request();
+		HttpServerResponse response = routingContext.response();
+		JsonObject queryJson = new JsonObject();
+		queryJson.put("__instance-id", request.getParam("instanceid"));
+		// Query database for config
+		JsonObject resultJson = new JsonObject();
+		// Store response from DB to resultJson
+		response.setStatusCode(200);
+		response.headers().add("Content-Type", "JSON").add("Content-Length",
+				String.valueOf(resultJson.toString().length()));
+		response.write(resultJson.toString());
+		response.end();
+	}
+
+	public void setConfig(RoutingContext routingContext) {
+		HttpServerRequest request = routingContext.request();
+		HttpServerResponse response = routingContext.response();
+		MultiMap params = request.params();
+		JsonObject queryJson = new JsonObject();
+		for (String str : params.names()) {
+			queryJson.put(str, params.get(str));
+//			System.out.println(params.get(str));
+//			System.out.println("----");
+		}
+		// Query database for setting config
+		JsonObject resultJson = new JsonObject();
+		// Store response from DB to resultJson
+		response.setStatusCode(200);
+		response.headers().add("Content-Type", "JSON").add("Content-Length",
+				String.valueOf(resultJson.toString().length()));
+		response.write(resultJson.toString());
+		response.end();
+	}
+
+	public void deleteConfig(RoutingContext routingContext) {
+		HttpServerRequest request = routingContext.request();
+		HttpServerResponse response = routingContext.response();
+		JsonObject queryJson = new JsonObject();
+		queryJson.put("__instance-id", request.getParam("instanceid"));
+		System.out.println(queryJson.toString());
+		// Query database to delete Config
+		JsonObject resultJson = new JsonObject();
+		// Store response from DB to resultJson
+		response.setStatusCode(200);
+		response.headers().add("Content-Type", "JSON").add("Content-Length",
+				String.valueOf(resultJson.toString().length()));
+		response.write(resultJson.toString());
+		response.end();
+	}
+
+	public void updateConfig(RoutingContext routingContext) {
+		HttpServerRequest request = routingContext.request();
+		HttpServerResponse response = routingContext.response();
+		MultiMap params = request.params();
+		JsonObject queryJson = new JsonObject();
+		for (String str : params.names()) {
+			queryJson.put(str, params.get(str));
+//			System.out.println(params.get(str));
+//			System.out.println("----");
+		}
+		// Query database to update config
+		JsonObject resultJson = new JsonObject();
+		// Store response from DB to resultJson
+		response.setStatusCode(200);
+		response.headers().add("Content-Type", "JSON").add("Content-Length",
+				String.valueOf(resultJson.toString().length()));
+		response.write(resultJson.toString());
+		response.end();
+	}
+
+	public void appendConfig(RoutingContext routingContext) {
+		HttpServerRequest request = routingContext.request();
+		HttpServerResponse response = routingContext.response();
+		MultiMap params = request.params();
+		JsonObject queryJson = new JsonObject();
+		for (String str : params.names()) {
+			queryJson.put(str, params.get(str));
+//			System.out.println(params.get(str));
+//			System.out.println("----");
+		}
+		// Query database to append config
+		JsonObject resultJson = new JsonObject();
+		// Store response from DB to resultJson
+		response.setStatusCode(200);
+		response.headers().add("Content-Type", "JSON").add("Content-Length",
+				String.valueOf(resultJson.toString().length()));
+		response.write(resultJson.toString());
+		response.end();
+	}
 }
