b3a80743f3a4180781124fc2ea1a041c800c75e5
Mahidhar <mahidhar.chellamani@datakaveri.org>
2020-07-08 14:51:07 +0530

    Add Constants class for authenticator package

---
+++ src/main/java/iudx/catalogue/server/authenticator/Constants.java
@@ -0,0 +1,7 @@
+package iudx.catalogue.server.authenticator;
+
+public class Constants {
+    public static final String CONFIG_FILE = "config.properties";
+    public static final String KEYSTORE_PATH = "keystore";
+    public static final String KEYSTORE_PASSWORD = "keystorePassword";
+}
