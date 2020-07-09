4e1a463cc5be6ca002c512aa3c86c3d47c9e49f9
Mahidhar <mahidhar.chellamani@datakaveri.org>
2020-07-09 15:54:42 +0530

    Add failing unit test to check if auth server webclient is working

--- src/main/java/iudx/catalogue/server/authenticator/Constants.java
+++ src/main/java/iudx/catalogue/server/authenticator/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static final String CONFIG_FILE = "config.properties";
     public static final String KEYSTORE_PATH = "keystore";
     public static final String KEYSTORE_PASSWORD = "keystorePassword";
+    public static final String AUTH_SERVER_HOST = "auth.iudx.org.in";
+    public static final String AUTH_CERTINFO_PATH = "/auth/v1/certificate-info";
 }
--- src/test/java/iudx/catalogue/server/authenticator/AuthenticationServiceTest.java
+++ src/test/java/iudx/catalogue/server/authenticator/AuthenticationServiceTest.java
@@ -1,15 +1,21 @@
 package iudx.catalogue.server.authenticator;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.core.net.JksOptions;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 @ExtendWith(VertxExtension.class)
@@ -39,5 +45,40 @@ public class AuthenticationServiceTest {
     public void shouldSucceed(VertxTestContext testContext) {
         logger.info("Default test is passing");
         testContext.completeNow();
+    }
+
+    @Test
+    @DisplayName("Test if WebClient has been initialized correctly")
+    public void testWebClientSetup(VertxTestContext testContext) {
+//        WebClient client = AuthenticationVerticle.createWebClient(vertxObj, properties, true);
+
+        try {
+            FileInputStream configFile = new FileInputStream(Constants.CONFIG_FILE);
+            if (properties.isEmpty()) properties.load(configFile);
+        } catch (IOException e) {
+            logger.error("Could not load properties from config file", e);
+        }
+
+        WebClientOptions webClientOptions = new WebClientOptions();
+        webClientOptions
+                .setTrustAll(true)
+                .setSsl(true)
+                .setKeyStoreOptions(new JksOptions()
+                        .setPath(properties.getProperty(Constants.KEYSTORE_PATH))
+                        .setPassword(properties.getProperty(Constants.KEYSTORE_PASSWORD)));
+        WebClient client = WebClient.create(vertxObj, webClientOptions);
+
+        client.post(Constants.AUTH_SERVER_HOST, Constants.AUTH_CERTINFO_PATH).send(httpResponseAsyncResult -> {
+            if (httpResponseAsyncResult.failed()) {
+                logger.error("Cert info call to auth server failed");
+                testContext.failNow(httpResponseAsyncResult.cause());
+                return;
+            }
+            logger.info("Cert info call to auth server succeeded");
+            logger.info("Response status code: " + httpResponseAsyncResult.result().statusCode());
+            JsonObject responseBody = httpResponseAsyncResult.result().bodyAsJsonObject();
+            logger.info("Response body: " + responseBody.toString());
+            testContext.completeNow();
+        });
     }
 }
