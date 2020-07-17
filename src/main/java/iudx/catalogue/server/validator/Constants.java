f6f2b6e408a0b6da8e467544c5ffa1ca047fe576
Karun <karun.singh@datakaveri.org>
2020-07-17 15:55:42 +0530

    added validator

---
+++ src/main/java/iudx/catalogue/server/validator/Constants.java
@@ -0,0 +1,13 @@
+package iudx.catalogue.server.validator;
+
+public class Constants {
+  public static final String REQUEST_GET = "GET";
+  public static final String CAT_TEST_SEARCH_INDEX = "cat/_search";
+  public static final String SOURCE = "_source";
+  public static final String QUERY_KEY = "query";
+  public static final String TERM = "term";
+  public static final String ID_KEYWORD = "id.keyword";
+  public static final String HITS = "hits";
+  public static final String TOTAL = "total";
+  public static final String VALUE = "value";
+}
