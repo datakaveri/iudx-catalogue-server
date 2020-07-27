655b774f196227002becb8c304a38ca13d641bd9
Karun <karun.singh@datakaveri.org>
2020-07-27 11:09:33 +0530

    validation unit tests

---
+++ src/test/java/iudx/catalogue/server/validator/ValidatorServiceTest.java
@@ -0,0 +1,163 @@
+package iudx.catalogue.server.validator;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import iudx.catalogue.server.database.Constants;
+import iudx.catalogue.server.database.DatabaseServiceTest;
+
+@ExtendWith(VertxExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ValidatorServiceTest {
+
+  private static Logger logger = LoggerFactory.getLogger(DatabaseServiceTest.class);
+  private static ValidatorService validator;
+  private static Vertx vertxObj;
+  private static RestClient client;
+  private static String databaseIP;
+  private static int databasePort;
+  private static Properties properties;
+  private static InputStream inputstream;
+
+  @BeforeAll
+  @DisplayName("Deploying Verticle")
+  static void startVertx(Vertx vertx, VertxTestContext testContext) {
+    vertxObj = vertx;
+    /* Read the configuration and set the rabbitMQ server properties. */
+    properties = new Properties();
+    inputstream = null;
+
+    try {
+
+      inputstream = new FileInputStream(Constants.CONFIG_FILE);
+      properties.load(inputstream);
+
+      databaseIP = properties.getProperty(Constants.DATABASE_IP);
+      databasePort = Integer.parseInt(properties.getProperty(Constants.DATABASE_PORT));
+
+    } catch (Exception ex) {
+
+      logger.info(ex.toString());
+    }
+
+    // TODO : Need to enable TLS using xpack security
+    client = RestClient.builder(new HttpHost(databaseIP, databasePort, "http")).build();
+    validator = new ValidatorServiceImpl(client);
+    testContext.completeNow();
+  }
+
+  @AfterEach
+  public void finish(VertxTestContext testContext) {
+    logger.info("Finishing....");
+    vertxObj.close(testContext.succeeding(response -> testContext.completeNow()));
+  }
+
+  @Test
+  @Order(1)
+  @DisplayName("Test Link Validation")
+  void validLinkTest(VertxTestContext testContext) {
+    JsonObject request = new JsonObject();
+    request
+        .put("type", new JsonArray().add("iudx:Resource"))
+        .put(
+            "id",
+            "rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531/rs.iudx.org.in/sensors/sensorA")
+        .put(
+            "resourceGroup",
+            "rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531/rs.iudx.org.in/sensors");
+    validator.validateItem(
+        request,
+        testContext.succeeding(
+            response ->
+                testContext.verify(
+                    () -> {
+                      testContext.completeNow();
+                    })));
+  }
+
+  @Test
+  @Order(2)
+  @DisplayName("Test Invalid Link")
+  void invalidLinkTest(VertxTestContext testContext) {
+    JsonObject request = new JsonObject();
+    request
+        .put("type", new JsonArray().add("iudx:Resource"))
+        .put(
+            "id",
+            "rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531/rs.iudx.org.in/sensors/sensorA")
+        .put(
+            "resourceGroup",
+            "rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531/rs.iudx.org.in/sensors123");
+    validator.validateItem(
+        request,
+        testContext.failing(
+            response ->
+                testContext.verify(
+                    () -> {
+                      testContext.completeNow();
+                    })));
+  }
+
+  @Test
+  @Order(3)
+  @DisplayName("Valid Schema Test")
+  void validSchemaTest(VertxTestContext testContext) {
+    try {
+      JsonObject request =
+          new JsonObject(
+              "{\"@context\": \"https://voc.iudx.org.in/\",\"type\": [\"iudx:ResourceGroup\"],\"id\":\"rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531/rs.iudx.org.in/sensors\",\"description\": \"Description of this resource group\",\"name\": \"sensors\",\"tags\": \"sensor, sensing, resource, battery operated\",\"itemStatus\": \"ACTIVE\",\"provider\": \"rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531\",\"resourceServer\": \"rbccps.org/00D75505FD5256B142AFD9C0E32790FA7180D500/rs.iudx.org.in\",\"resourceAuthControlLevel\": \"INDIVIDUAL\",\"resourceType\": \"messageStream\",\"authServerInfo\": {\"type\": [\"AuthServerInfoValue\"],\"authServerURL\": \"https://auth.iudx.org.in\",\"authType\": \"iudx-auth\"},\"accessObjectInfo\":{\"type\": [\"AccessObjectInfoValue\"],\"accessObject\": \"https://example.com/sensorsApis.json\",\"additionalInfoURL\": \"https://example.com/sensorsApis\",\"accessObjectType\": \"openAPI\"},\"iudxResourceAPIs\": [\"attribute\", \"temporal\"],\"itemCreatedAt\": \"2019-02-20T10:30:06.093121\",\"location\": {\"type\": \"Place\",\"address\": \"Bangalore\"}}");
+      System.out.println(request.toString());
+      validator.validateSchema(
+          request,
+          testContext.succeeding(
+              response ->
+                  testContext.verify(
+                      () -> {
+                        testContext.completeNow();
+                      })));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  @Order(4)
+  @DisplayName("Invalid Schema Test")
+  void invalidSchemaTest(VertxTestContext testContext) {
+    try {
+      JsonObject request =
+          new JsonObject(
+              "{\"id\":\"rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531/rs.iudx.org.in/sensors\",\"description\": \"Description of this resource group\",\"name\": \"sensors\",\"tags\": \"sensor, sensing, resource, battery operated\",\"itemStatus\": \"ACTIVE\",\"provider\": \"rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531\",\"resourceServer\": \"rbccps.org/00D75505FD5256B142AFD9C0E32790FA7180D500/rs.iudx.org.in\",\"resourceAuthControlLevel\": \"INDIVIDUAL\",\"resourceType\": \"messageStream\",\"authServerInfo\": {\"type\": [\"AuthServerInfoValue\"],\"authServerURL\": \"https://auth.iudx.org.in\",\"authType\": \"iudx-auth\"},\"accessObjectInfo\":{\"type\": [\"AccessObjectInfoValue\"],\"accessObject\": \"https://example.com/sensorsApis.json\",\"additionalInfoURL\": \"https://example.com/sensorsApis\",\"accessObjectType\": \"openAPI\"},\"iudxResourceAPIs\": [\"attribute\", \"temporal\"],\"itemCreatedAt\": \"2019-02-20T10:30:06.093121\",\"location\": {\"type\": \"Place\",\"address\": \"Bangalore\"}}");
+      System.out.println(request.toString());
+      validator.validateSchema(
+          request,
+          testContext.failing(
+              response ->
+                  testContext.verify(
+                      () -> {
+                        testContext.completeNow();
+                      })));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
