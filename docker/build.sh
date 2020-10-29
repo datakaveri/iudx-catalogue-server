1a9baa0f85af6be7f44dacffd1a556acbad79a39
rraks <rakshitadmar@gmail.com>
2020-10-29 14:40:33 +0530

    First stab at docker based tests

--- docker/build.sh
+++ docker/build.sh
@@ -3,3 +3,4 @@
 # To be executed from project root
 docker build -t iudx/cat-depl:latest -f docker/depl.dockerfile .
 docker build -t iudx/cat-dev:latest -f docker/dev.dockerfile .
+docker build -t iudx/cat-test:latest -f docker/test.dockerfile .
