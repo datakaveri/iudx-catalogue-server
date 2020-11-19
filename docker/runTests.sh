164b5305a28dcd9452d3115ee232c782f80a42fa
karun-singh <66350877+karun-singh@users.noreply.github.com>
2020-11-19 11:40:55 +0530

    Update runTests.sh

--- docker/runTests.sh
+++ docker/runTests.sh
@@ -3,3 +3,6 @@
 nohup mvn clean compile test-compile exec:java@catalogue-server & 
 sleep 20
 mvn test -Dtest=ApiServerVerticlePreprareTest
+mvn test -Dtest=ApiServerVerticleTest
+mvn test -Dtest=DatabaseServiceTest
+mvn test -Dtest=ServerVerticleDeboardTest
