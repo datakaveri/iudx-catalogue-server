2c4914ddf8e5cabc4fac23d8d26240ce78403bc6
karun97 <75062762+karun97@users.noreply.github.com>
2020-11-26 12:28:39 +0530

    Update runTests.sh

--- docker/runTests.sh
+++ docker/runTests.sh
@@ -3,6 +3,3 @@
 nohup mvn clean compile test-compile exec:java@catalogue-server & 
 sleep 20
 mvn clean test-compile surefire:test surefire-report:report -Dtest=ApiServerVerticlePreprareTest
-mvn clean test-compile surefire:test surefire-report:report -Dtest=ApiServerVerticleTest
-mvn clean test-compile surefire:test surefire-report:report -Dtest=DatabaseServiceTest
-mvn clean test-compile surefire:test surefire-report:report -Dtest=ServerVerticleDeboardTest
