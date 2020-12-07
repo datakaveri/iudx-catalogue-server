ed7b2b0fc1baf726044c47edf94f1fcd1934f4ce
rraks <rakshitadmar@gmail.com>
2020-12-07 12:15:31 +0530

    More limits on geobounds. Modified docker image names to avoid confusion

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # To be executed from project root
-docker build -t iudx/cat-depl:latest -f docker/depl.dockerfile .
+docker build -t iudx/cat-prod:latest -f docker/prod.dockerfile .
 docker build -t iudx/cat-dev:latest -f docker/dev.dockerfile .
 docker build -t iudx/cat-test:latest -f docker/test.dockerfile .
