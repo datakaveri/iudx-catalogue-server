d571ccd0e8b44ae5d0c79004d351a770b3b6d9ef
karun-singh <66350877+karun-singh@users.noreply.github.com>
2021-01-21 15:15:46 +0530

    Update runTests.sh

--- docker/runTests.sh
+++ docker/runTests.sh
@@ -3,10 +3,10 @@
 nohup mvn clean compile test-compile exec:java@catalogue-server & 
 sleep 20
 mvn test-compile surefire:test surefire-report:report -Dtest=ApiServerVerticlePreprareTest
-mv target/jacoco.exec ApiServerVerticlePreprareTest.exec
+mv target/jacoco.exec target/ApiServerVerticlePreprareTest.exec
 mvn test-compile surefire:test surefire-report:report -Dtest=ApiServerVerticleTest
-mv target/jacoco.exec ApiServerVerticleTest.exec
+mv target/jacoco.exec target/ApiServerVerticleTest.exec
 mvn test-compile surefire:test surefire-report:report -Dtest=DatabaseServiceTest
-mv target/jacoco.exec DatabaseServiceTest.exec
+mv target/jacoco.exec target/DatabaseServiceTest.exec
 mvn test-compile surefire:test surefire-report:report -Dtest=ServerVerticleDeboardTest
-mv target/jacoco.exec ServerVerticleDeboardTest.exec
+mv target/jacoco.exec target/ServerVerticleDeboardTest.exec
