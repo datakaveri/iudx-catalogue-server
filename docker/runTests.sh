77e3c3aa3c142429dbd1ebe4d1a6f64e7e63ce13
karun-singh <66350877+karun-singh@users.noreply.github.com>
2021-11-26 17:02:23 +0530

    test changes for jacoco

--- docker/runTests.sh
+++ docker/runTests.sh
@@ -2,16 +2,14 @@
 
 nohup mvn clean compile exec:java@catalogue-server & 
 sleep 20
-mvn clean test-compile
-mv target/jacoco.exec target/jacoco2.exec
 mvn test surefire:test surefire-report:report -Dtest=ServerVerticleDeboardTest
 mvn test surefire:test surefire-report:report -Dtest=ApiServerVerticlePreprareTest
-#mv target/jacoco.exec target/ApiServerVerticlePreprareTest.exec
+mv target/jacoco.exec target/ApiServerVerticlePreprareTest.exec
 mvn test surefire:test surefire-report:report -Dtest=ApiServerVerticleTest
-#mv target/jacoco.exec target/ApiServerVerticleTest.exec
+mv target/jacoco.exec target/ApiServerVerticleTest.exec
 #mvn test-compile surefire:test surefire-report:report -Dtest=DatabaseServiceTest
 #mv target/jacoco.exec target/DatabaseServiceTest.exec
 #mvn test-compile surefire:test surefire-report:report -Dtest=ConstraintsValidationTest,AuthenticationServiceTest,ElasticClientTest,QueryDecoderTest,SummarizerTest,ValidatorServiceTest
 #mv target/jacoco.exec target/jacoco2.exec
 mvn test surefire:test surefire-report:report -Dtest=ServerVerticleDeboardTest
-#mv target/jacoco.exec target/ServerVerticleDeboardTest.exec
+mv target/jacoco.exec target/ServerVerticleDeboardTest.exec
