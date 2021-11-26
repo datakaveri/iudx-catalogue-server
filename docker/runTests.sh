1390c48f21df36314578ee26efa4701bd9b91c7c
karun-singh <krsraj22@gmail.com>
2021-11-26 11:25:33 +0530

    changed mvn command to run tests

--- docker/runTests.sh
+++ docker/runTests.sh
@@ -2,14 +2,14 @@
 
 nohup mvn clean compile exec:java@catalogue-server & 
 sleep 20
-mvn test-compile surefire:test surefire-report:report -Dtest=ServerVerticleDeboardTest
-mvn test-compile surefire:test surefire-report:report -Dtest=ApiServerVerticlePreprareTest
+mvn clean test -Dtest=ServerVerticleDeboardTest
+mvn clean test -Dtest=ApiServerVerticlePreprareTest
 mv target/jacoco.exec target/ApiServerVerticlePreprareTest.exec
-mvn test-compile surefire:test surefire-report:report -Dtest=ApiServerVerticleTest
+mvn clean test -Dtest=ApiServerVerticleTest
 mv target/jacoco.exec target/ApiServerVerticleTest.exec
 #mvn test-compile surefire:test surefire-report:report -Dtest=DatabaseServiceTest
 #mv target/jacoco.exec target/DatabaseServiceTest.exec
 #mvn test-compile surefire:test surefire-report:report -Dtest=ConstraintsValidationTest,AuthenticationServiceTest,ElasticClientTest,QueryDecoderTest,SummarizerTest,ValidatorServiceTest
 #mv target/jacoco.exec target/jacoco2.exec
-mvn test-compile surefire:test surefire-report:report -Dtest=ServerVerticleDeboardTest
+mvn clean test -Dtest=ServerVerticleDeboardTest
 mv target/jacoco.exec target/ServerVerticleDeboardTest.exec
