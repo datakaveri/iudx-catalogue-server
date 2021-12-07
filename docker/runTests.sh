0c0918870f56bfb659b3e28c0eeb81669c98bb62
karun-singh <66350877+karun-singh@users.noreply.github.com>
2021-12-07 18:45:20 +0530

    Update runTests.sh

--- docker/runTests.sh
+++ docker/runTests.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-set -ex
 
 nohup mvn clean compile exec:java@catalogue-server & 
 sleep 20
-mvn test surefire:test surefire-report:report -Dtest=ServerVerticleDeboardTest
-mvn test surefire:test surefire-report:report -Dtest=ApiServerVerticlePreprareTest
-mv target/jacoco.exec target/ApiServerVerticlePreprareTest.exec
-mvn test surefire:test surefire-report:report -Dtest=ApiServerVerticleTest
-mv target/jacoco.exec target/ApiServerVerticleTest.exec
-#mvn test-compile surefire:test surefire-report:report -Dtest=DatabaseServiceTest
-#mv target/jacoco.exec target/DatabaseServiceTest.exec
-#mvn test-compile surefire:test surefire-report:report -Dtest=ConstraintsValidationTest,AuthenticationServiceTest,ElasticClientTest,QueryDecoderTest,SummarizerTest,ValidatorServiceTest
-#mv target/jacoco.exec target/jacoco2.exec
-# mvn test surefire:test surefire-report:report -Dtest=ServerVerticleDeboardTest
-# mv target/jacoco.exec target/ServerVerticleDeboardTest.exec
+mvn test-compile surefire:test surefire-report:report -Dtest=ServerVerticleDeboardTest
+mvn test-compile surefire:test surefire-report:report -Dtest=ApiServerVerticlePreprareTest
+mvn test-compile surefire:test surefire-report:report -Dtest=ApiServerVerticleTest
+mvn test-compile surefire:test surefire-report:report -Dtest=DatabaseServiceTest
+mv target/jacoco.exec target/DatabaseServiceTest.exec
+mvn test-compile surefire:test surefire-report:report -Dtest=ConstraintsValidationTest,AuthenticationServiceTest,ElasticClientTest,QueryDecoderTest,SummarizerTest,ValidatorServiceTest
+mv target/jacoco.exec target/jacoco2.exec
+mvn test-compile surefire:test surefire-report:report -Dtest=ServerVerticleDeboardTest
