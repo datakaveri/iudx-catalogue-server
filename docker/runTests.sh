749522169b8577d3ff1ea1885bf5e068a77f83b6
karun-singh <karun.singh@datakaveri.org>
2022-08-23 11:48:17 +0530

    including code cov from apiServer tests

--- docker/runTests.sh
+++ docker/runTests.sh
@@ -3,10 +3,12 @@
 nohup mvn clean compile exec:java@catalogue-server & 
 sleep 40
 mvn clean test -Dtest=ServerVerticleDeboardTest
+mv target/jacoco.exec target/ServerVerticleDeboardTest.exec
 mvn test -Dtest=ApiServerVerticlePreprareTest
+mv target/jacoco.exec target/ApiServerVerticlePreprareTest.exec
 mvn test -Dtest=ApiServerVerticleTest
+mv target/jacoco.exec target/ApiServerVerticleTest.exec
 mvn test -Dtest=ServerVerticleDeboardTest
 mvn test -Dtest=DatabaseServiceTest
 mv target/jacoco.exec target/DatabaseServiceTest.exec
 mvn test -Dtest=ConstraintsValidationTest,AuthenticationServiceTest,ElasticClientTest,QueryDecoderTest,SummarizerTest,ValidatorServiceTest,RatingServiceTest,DatabrokerServiceTest
-mv target/jacoco.exec target/jacoco2.exec
