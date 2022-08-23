f03a86f2eb1a1d01e09d52c96b81535fccc15e19
karun-singh <karun.singh@datakaveri.org>
2022-08-23 16:39:58 +0530

    added unit test classes

--- docker/runTests.sh
+++ docker/runTests.sh
@@ -3,17 +3,7 @@
 nohup mvn clean compile exec:java@catalogue-server & 
 sleep 40
 mvn clean test -Dtest=ServerVerticleDeboardTest
-mv target/jacoco.exec target/ServerVerticleDeboardTest.exec
 mvn test -Dtest=ApiServerVerticlePreprareTest
-mv target/jacoco.exec target/ApiServerVerticlePreprareTest.exec
 mvn test -Dtest=ApiServerVerticleTest
-mv target/jacoco.exec target/ApiServerVerticleTest.exec
 mvn test -Dtest=ServerVerticleDeboardTest
-mvn test -Dtest=DatabaseServiceTest
-mv target/jacoco.exec target/DatabaseServiceTest.exec
-<<<<<<< HEAD
-mvn test -Dtest=ConstraintsValidationTest,AuthenticationServiceTest,ElasticClientTest,QueryDecoderTest,SummarizerTest,ValidatorServiceTest,RatingServiceTest,DatabrokerServiceTest,AuthenticationServiceImplTest,NLPSearchServiceImplTest,ValidatorServiceImplTest,ExceptionHandlerTest,QueryMapperTest,RespBuilderTest,QueryBuilderTest,AuthorizationRequestTest,JwtDataTest
-mv target/jacoco.exec target/jacoco2.exec
-=======
-mvn test -Dtest=ConstraintsValidationTest,AuthenticationServiceTest,ElasticClientTest,QueryDecoderTest,SummarizerTest,ValidatorServiceTest,RatingServiceTest,DatabrokerServiceTest
->>>>>>> including code cov from apiServer tests
+mvn test -Dtest=ConstraintsValidationTest,ExceptionHandlerTest,QueryMapperTest,RespBuilderTest,QueryBuilderTest,AuthorizationRequestTest,JwtDataTest,DatabaseServiceImplTest,ElasticClientTest,QueryDecoderTest,SummarizerTest,DataBrokerServiceTest,GeocodingServiceTest,RatingServiceTest,ValidatorServiceTest,AuthenticationServiceTest,ValidatorServiceTest,RatingServiceTest,AuthenticationServiceImplTest,NLPSearchServiceImplTest,ValidatorServiceImplTest,AuditingServiceTest,JwtAuthServiceImplTest
