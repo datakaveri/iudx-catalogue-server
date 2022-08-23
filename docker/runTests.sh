edff242ec8128df3aece6ed49f6baa95aaff6b2a
karun-singh <karun.singh@datakaveri.org>
2022-08-23 16:39:58 +0530

    added unit test classes

--- docker/runTests.sh
+++ docker/runTests.sh
@@ -3,12 +3,7 @@
 nohup mvn clean compile exec:java@catalogue-server & 
 sleep 40
 mvn clean test -Dtest=ServerVerticleDeboardTest
-mv target/jacoco.exec target/ServerVerticleDeboardTest.exec
 mvn test -Dtest=ApiServerVerticlePreprareTest
-mv target/jacoco.exec target/ApiServerVerticlePreprareTest.exec
 mvn test -Dtest=ApiServerVerticleTest
-mv target/jacoco.exec target/ApiServerVerticleTest.exec
 mvn test -Dtest=ServerVerticleDeboardTest
-mvn test -Dtest=DatabaseServiceTest
-mv target/jacoco.exec target/DatabaseServiceTest.exec
-mvn test -Dtest=ConstraintsValidationTest,AuthenticationServiceTest,ElasticClientTest,QueryDecoderTest,SummarizerTest,ValidatorServiceTest,RatingServiceTest,DatabrokerServiceTest
+mvn test -Dtest=ConstraintsValidationTest,ExceptionHandlerTest,QueryMapperTest,RespBuilderTest,AuditingServiceTest,QueryBuilderTest,JwtAuthServiceImplTest,AuthorizationRequestTest,JwtDataTest,DatabaseServiceTest,ElasticClientTest,QueryDecoderTest,SummarizerTest,DataBrokerServiceTest,GeocodingServiceTest,NLPSearchServiceTest,RatingServiceTest,ValidatorServiceTest
