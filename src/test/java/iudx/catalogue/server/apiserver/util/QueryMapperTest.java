a46c076379414f9e04c1bdc69768fbc8026fb15e
Tanvi Prasad <tanvi.prasad@datakaveri.org>
2022-09-15 10:13:16 +0530

    postman test added

--- src/test/java/iudx/catalogue/server/apiserver/util/QueryMapperTest.java
+++ src/test/java/iudx/catalogue/server/apiserver/util/QueryMapperTest.java
@@ -13,8 +13,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
-
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static iudx.catalogue.server.util.Constants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -172,6 +177,116 @@ public class QueryMapperTest {
         jsonArray.add("dummy");
         requestBody.put(INSTANCE,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the j");
         assertEquals( new JsonObject().put(STATUS,FAILED).put(DESC,"The max length of 'instance' should be " + INSTANCE_SIZE), QueryMapper.validateQueryParam(requestBody));
+        vertxTestContext.completeNow();
+    }
+    @Test
+    @DisplayName("Test validateQueryParam method when Instance has exceeded the limit")
+    public void testValidateQueryParamInstanceLimit(VertxTestContext vertxTestContext) {
+        JsonObject requestBody=new JsonObject();
+        QueryMapper queryMapper=new QueryMapper();
+        JsonArray jsonArray=new JsonArray();
+        jsonArray.add("7.0000000");
+        requestBody.put(SEARCH_TYPE,SEARCH_TYPE_GEO);
+        requestBody.put(COORDINATES,jsonArray);
+        JsonObject errResponse = new JsonObject().put(STATUS, FAILED);
+        errResponse.put(DESC, "Invalid coordinate format");
+         assertEquals(errResponse,queryMapper.validateQueryParam(requestBody));
+        vertxTestContext.completeNow();
+    }
+    @Test
+    @DisplayName("Test validateQueryParam method when requestBody contains Geometry")
+    public void testValidateQueryParamGeometry(VertxTestContext vertxTestContext) {
+        JsonObject requestBody=new JsonObject();
+        QueryMapper queryMapper=new QueryMapper();
+        requestBody.put(SEARCH_TYPE,SEARCH_TYPE_GEO);
+        requestBody.put(GEOMETRY,POINT)
+                .put(MAX_DISTANCE,999999);
+        JsonObject errResponse = new JsonObject().put(STATUS, FAILED);
+        errResponse.put(DESC, "The 'maxDistance' should range between 0-10000m");
+        assertEquals(errResponse,queryMapper.validateQueryParam(requestBody));
+        vertxTestContext.completeNow();
+    }
+    @Test
+    @DisplayName("Test validateQueryParam method when searchType contains SEARCH_TYPE_TEXT")
+    public void testValidateQueryParamSearch_Type_Test(VertxTestContext vertxTestContext) {
+        JsonObject requestBody=new JsonObject();
+        QueryMapper queryMapper=new QueryMapper();
+        requestBody.put(SEARCH_TYPE,SEARCH_TYPE_TEXT);
+        requestBody.put(Q_VALUE,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the j");
+        JsonObject errResponse = new JsonObject().put(STATUS, FAILED);
+        errResponse.put(DESC, "The max string(q) size supported is " + STRING_SIZE);
+        assertEquals(errResponse,queryMapper.validateQueryParam(requestBody));
+        vertxTestContext.completeNow();
+    }
+    @Test
+    @DisplayName("Test validateQueryParam method when searchType contains SEARCH_TYPE_TEXT")
+    public void testValidateQueryResponse_Filter(VertxTestContext vertxTestContext) {
+        JsonObject requestBody=new JsonObject();
+        QueryMapper queryMapper=new QueryMapper();
+        JsonArray jsonArray=new JsonArray();
+        jsonArray.add("value1").add("value2").add("value3").add("value4").add("value5").add("value6").add("value7").add("value8").add("value9").add("value10").add("value11");
+        requestBody.put(SEARCH_TYPE,RESPONSE_FILTER);
+        requestBody.put(FILTER,jsonArray);
+        JsonObject errResponse = new JsonObject().put(STATUS, FAILED);
+        errResponse.put(DESC, "The max number of 'filter' should be " + FILTER_VALUE_SIZE);
+        assertEquals(errResponse,queryMapper.validateQueryParam(requestBody));
+        vertxTestContext.completeNow();
+    }
+    @Test
+    @DisplayName("Test validateQueryParam method when requestBody contains LIMIT and OFFSET")
+    public void testValidateQueryLimitOffset(VertxTestContext vertxTestContext) {
+        JsonObject requestBody=new JsonObject();
+        QueryMapper queryMapper=new QueryMapper();
+        requestBody.put(LIMIT,10000)
+               .put(OFFSET,10000);
+        JsonObject errResponse = new JsonObject().put(STATUS, FAILED);
+        errResponse.put(DESC, "The limit + offset should be between 1 to " + MAX_RESULT_WINDOW);
+        assertEquals(errResponse,queryMapper.validateQueryParam(requestBody));
+        vertxTestContext.completeNow();
+    }
+    @Test
+    @DisplayName("Test validateQueryParam method when searchType contains Search_Type_Attribute")
+    public void testValidateQuerySearch_Type_Attribute(VertxTestContext vertxTestContext) {
+        JsonObject requestBody=new JsonObject();
+        QueryMapper queryMapper=new QueryMapper();
+        JsonArray jsonArray=new JsonArray();
+        JsonArray jsonArray2=new JsonArray();
+        jsonArray2.add("value").add("value").add("value").add("value").add("value");
+        jsonArray.add("value");
+        requestBody.put(SEARCH_TYPE,SEARCH_TYPE_ATTRIBUTE)
+                .put(PROPERTY,jsonArray)
+                .put(VALUE,jsonArray2);
+        JsonObject errResponse = new JsonObject().put(STATUS, FAILED);
+        errResponse.put(DESC, "The max number of 'value' should be " + VALUE_SIZE);
+        assertEquals(errResponse,queryMapper.validateQueryParam(requestBody));
+        vertxTestContext.completeNow();
+    }
+    @Test
+    @DisplayName("Test validateQueryParam method when limit is exceeded")
+    public void testValidateQueryParamExceedLimit(VertxTestContext vertxTestContext) {
+        JsonObject requestBody=new JsonObject();
+        QueryMapper queryMapper=new QueryMapper();
+        JsonArray jsonArray=new JsonArray();
+        jsonArray.add("value1").add("value2").add("value3").add("value4").add("value5");
+        requestBody.put(SEARCH_TYPE,SEARCH_TYPE_ATTRIBUTE)
+                .put(PROPERTY,jsonArray);
+        JsonObject errResponse = new JsonObject().put(STATUS, FAILED);
+        errResponse.put(DESC, "The max number of 'property' should be " + PROPERTY_SIZE);
+        assertEquals(errResponse,queryMapper.validateQueryParam(requestBody));
+        vertxTestContext.completeNow();
+    }
+    @Test
+    @DisplayName("Test validateQueryParam method when Instance has exceeded the limit")
+    public void testValidateQueryParamMaxLimit(VertxTestContext vertxTestContext) {
+        JsonObject requestBody=new JsonObject();
+        QueryMapper queryMapper=new QueryMapper();
+        JsonArray jsonArray=new JsonArray();
+        jsonArray.add("7.0000000").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9").add("7.9");
+        requestBody.put(SEARCH_TYPE,SEARCH_TYPE_GEO);
+        requestBody.put(COORDINATES,jsonArray);
+        JsonObject errResponse = new JsonObject().put(STATUS, FAILED);
+        errResponse.put(DESC, "The max number of 'coordinates' value is " + COORDINATES_SIZE);
+        assertEquals(errResponse,queryMapper.validateQueryParam(requestBody));
         vertxTestContext.completeNow();
     }
 
