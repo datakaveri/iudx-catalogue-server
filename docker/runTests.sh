c693cd77d95119190be748f42a35f3760ef01942
karun-singh <karun.singh@datakaveri.org>
2022-09-28 13:50:09 +0530

    Updated UT to use mvn clean test

--- docker/runTests.sh
+++ docker/runTests.sh
@@ -2,8 +2,4 @@
 
 nohup mvn clean compile exec:java@catalogue-server & 
 sleep 40
-mvn clean test -Dtest=ServerVerticleDeboardTest
-mvn test -Dtest=ApiServerVerticlePreprareTest
-mvn test -Dtest=ApiServerVerticleTest
-mvn test -Dtest=ServerVerticleDeboardTest
-mvn test -Dtest=ConstraintsValidationTest,ExceptionHandlerTest,QueryMapperTest,RespBuilderTest,QueryBuilderTest,AuthorizationRequestTest,JwtDataTest,DatabaseServiceImplTest,ElasticClientTest,QueryDecoderTest,SummarizerTest,DataBrokerServiceTest,GeocodingServiceTest,RatingServiceTest,ValidatorServiceTest,AuthenticationServiceTest,ValidatorServiceTest,RatingServiceTest,AuthenticationServiceImplTest,NLPSearchServiceImplTest,ValidatorServiceImplTest,AuditingServiceTest,JwtAuthServiceImplTest
+mvn clean test
