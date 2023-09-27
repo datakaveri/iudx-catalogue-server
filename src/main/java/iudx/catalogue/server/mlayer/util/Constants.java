4e32950372824041d64592787d7e14e17ec2919a
pranavrd <pranav.doshetty@datakaveri.org>
2023-09-27 09:24:45 +0530

    chore: change table name of rs

--- src/main/java/iudx/catalogue/server/mlayer/util/Constants.java
+++ src/main/java/iudx/catalogue/server/mlayer/util/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
   public static final String INSTANCE_ID = "instanceId";
   public static final String DOMAIN_ID = "domainId";
   public static final String GET_HIGH_COUNT_DATASET =
-          "select resourcegroup, count(id) as totalhits from $1 "
-                  + "group by resourcegroup order by totalhits "
+          "select resource_group, count(id) as totalhits from $1 "
+                  + "group by resource_group order by totalhits "
                   + "desc limit 6";
 }
