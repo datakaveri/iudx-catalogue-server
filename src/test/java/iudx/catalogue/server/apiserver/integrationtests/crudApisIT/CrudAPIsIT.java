8a971da0ed4bffc1b0b8a43e584745f31566ca8e
Kranthi-Guribilli <kranthi.guribilli@datakaveri.org>
2024-01-03 18:42:02 +0530

    Update CrudAPIsIT.java

--- src/test/java/iudx/catalogue/server/apiserver/integrationtests/crudApisIT/CrudAPIsIT.java
+++ src/test/java/iudx/catalogue/server/apiserver/integrationtests/crudApisIT/CrudAPIsIT.java
@@ -3,7 +3,7 @@ package iudx.catalogue.server.apiserver.integrationtests.crudApisIT;
 import io.restassured.response.Response;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import iudx.catalogue.server.apiserver.integrationTests.RestAssuredConfiguration;
+import iudx.catalogue.server.apiserver.integrationtests.RestAssuredConfiguration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.*;
